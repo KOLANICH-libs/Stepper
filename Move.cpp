420f693ab2c90e97b2fcbe84526c72e32641c221
Abe Wieland <abermeister479@gmail.com>
2018-06-26 16:14:50 -0500

    Switched to full c++

--- Move.cpp
+++ Move.cpp
@@ -1,7 +1,7 @@
 #include "Stepper.h"
 #include "bcm2835.h"
 #include <unistd.h>
-#include <cstdlib>
+#include <iostream>
 
 #define STEPS_PER_REVOLUTION 512 //512 steps per revolution
 
@@ -11,17 +11,17 @@ int main()
   Stepper motor(STEPS_PER_REVOLUTION, 27, 22, 23, 24);
   // Set the speed
   motor.setSpeed(20);
-  printf("Speed set");
+  std::cout << "Speed set" << std::endl;
   while (1)
   {
     // Step the motor one revolution clockwise
     motor.step(STEPS_PER_REVOLUTION);
-    printf("Clockwise");
+    std::cout << "Clockwise" << std::endl;
     // Sleep to make moves independent
     usleep(1000000);
     // Step the motor one revolution counterclockwise
     motor.step(-STEPS_PER_REVOLUTION);
-    printf("Counterclockwise");
+    std::cout << "Counterclockwise" << std::endl;
     // Sleep to make moves independent
     usleep(1000000);
   }
